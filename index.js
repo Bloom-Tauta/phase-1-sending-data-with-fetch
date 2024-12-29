d27c022cb137c13f71f02a737477d5340ee1e953
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-12-29 17:06:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 // Add your code here
-fetch("")
-.then(resp => resp.json())
-.then(function(data) {
-    console.log(data)
-});
+document.addEventListener("DOMContentLoaded", submitData);
+
+function submitData(name, email) {
+    const inputObj = {
+        name: name,
+        email: email
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(inputObj)
+    })
+    .then(res => res.json())
+    .then(data => appendIdToDOM(data.id))
+    .catch(error => appendErrorToDOM(error.message))
+}
+
+function appendIdToDOM(id) {
+    const body = document.querySelector('body');
+    const p = document.createElement('p');
+    p.textContent = `New ID: ${id}`;
+    body.appendChild(p);
+  }
+  
+  function appendErrorToDOM(errorMessage) {
+    const body = document.querySelector('body');
+    const p = document.createElement('p');
+    p.textContent = `Error: ${errorMessage}`;
+    body.appendChild(p);
+  }
